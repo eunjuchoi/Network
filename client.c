c1a8d54e56371f060f34cced653d7e66daeb3c70
ds <ds@ds-VirtualBox.(none)>
2015-06-03 02:53:06 +0900

    file transfer

---
+++ client.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+	
+#define BUF_SIZE 128
+#define NAME_SIZE 20
+	
+void * send_msg(void * arg);
+void error_handling(char * msg);
+	
+char name[NAME_SIZE]= {NULL};
+char msg[BUF_SIZE] = {NULL};
+int setFName = 0;
+//pthread_mutex_t mutx;
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	struct sockaddr_in serv_addr;
+	pthread_t snd_thread, rcv_thread;
+	void * thread_return;
+	if(argc!=4) {
+		printf("You have to write %s <IP> <port> <name>\n", argv[0]);
+		exit(1);
+	 }
+	
+	//pthread_mutex_init(&mutx, NULL);
+	// pthread_mutex_lock(&mutx);
+	// pthread_mutex_unlock(&mutx); 
+	// 뮤텍스 이제 이용가능
+
+	sprintf(name, "%s", argv[3]);
+	sock=socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	  
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error");
+
+	write(sock, name, NAME_SIZE);
+	// ㄴ 이름을 서버쪽으로 보냄.
+
+	printf("\n\n");
+	printf("CONNECTING..... \n<Menu>\n1.credit\n2.send\n3.exit\n");
+
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_join(snd_thread, &thread_return);
+	close(sock);  
+	return 0;
+}
+void * send_msg(void * arg)   // send thread main
+{
+	int sock=*((int*)arg);
+	int Flength = 0;
+	int i=0;
+	int fSize = 0;
+	int fEnd = 0;
+	int id = 0;
+	char name_msg[NAME_SIZE+BUF_SIZE] = {NULL};
+	char t_msg[BUF_SIZE] = {NULL};
+	char last_msg[BUF_SIZE] = {NULL};
+	char t_name_msg[BUF_SIZE] = {NULL};
+	char noUse[BUF_SIZE] = {NULL};
+	const char enter[BUF_SIZE] = {"\n"};
+	const char whisper[BUF_SIZE] = {"/whisper\n"};
+
+	// ㄴ (진행중)
+
+
+	while(1) 
+	{
+		fgets(msg, BUF_SIZE, stdin);
+		
+		////////////////////////////////////////////////////////exit
+		if(!strcmp(msg,"exit\n")) 
+		{
+			close(sock);
+			exit(0);
+		}
+
+		////////////////////////////////////////////////////////send
+		if(!strcmp(msg,"send\n")) 
+		{
+			char filename[BUF_SIZE];
+			char setfname[BUF_SIZE];
+			FILE *fp;
+			FILE *size;
+
+			//Select file
+			printf("Write File Name : ");
+			scanf("%s", filename);
+
+			size = fopen(filename, "rb");
+			if(size == NULL) {
+				printf("There is no file named %s\n",filename);
+				continue;
+			}
+
+			//transfer send signal
+			write(sock, "send file", BUF_SIZE);
+
+			//file size
+			while(1) {	
+				fEnd = fread(noUse, 1 , BUF_SIZE, size);
+				fSize += fEnd;
+
+				if(fEnd != BUF_SIZE)
+					break;
+			}
+			fclose(size);
+
+			//transfer file size
+			printf("File transfer start \n(File Size : %d Byte)\n", fSize); 
+			write(sock, &fSize, sizeof(int)); // 파일 크기정보 먼저 보냄.
+			fSize = 0;
+
+			//transfer setfname
+			printf("set file name : ");
+			scanf("%s",&setfname);
+			write(sock,setfname,BUF_SIZE);
+			
+			//transfer file
+			fp = fopen(filename, "rb");
+			while(1) {		
+				Flength = fread(t_msg, 1 , BUF_SIZE, fp);
+
+				if(Flength != BUF_SIZE) {
+					for(i=0; i<Flength; i++) {
+						last_msg[i] = t_msg[i];
+					} 
+					write(sock, last_msg, BUF_SIZE);
+					write(sock, "file end", BUF_SIZE);
+					break;
+				}
+				write(sock, t_msg, BUF_SIZE); 
+			}
+			fclose(fp);
+			printf("File transfer finish \n");			
+		}
+
+		////////////////////////////////////////////////////////credit
+		if(!strcmp(msg,"credit\n"))
+		{
+			printf("Write ID :");
+			scanf("%d", &id);
+			switch(id)
+			{
+			case 20123423:
+				printf("hi\n");
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	return NULL;
+}
+void error_handling(char *msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ server.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define BUF_SIZE 128
+#define MAX_CLNT 2
+#define NAME_SIZE 20
+
+void * handle_clnt(void * arg);
+void error_handling(char * msg);
+
+int clnt_cnt=0;
+int clnt_socks[MAX_CLNT];
+char clnt_name[NAME_SIZE]= {NULL};
+char clnt_names[MAX_CLNT][NAME_SIZE]= {NULL};
+
+
+pthread_mutex_t mutx;
+
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock, i;
+	struct sockaddr_in serv_adr, clnt_adr;
+	int clnt_adr_sz;
+	pthread_t t_id;
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+  
+	pthread_mutex_init(&mutx, NULL);
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET; 
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+	
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	while(1)
+	{
+		clnt_adr_sz=sizeof(clnt_adr);
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+	
+		if(clnt_cnt >= MAX_CLNT) {
+			printf("CONNECT FAIL : %d \n", clnt_sock);
+			write(clnt_sock, "too many users. sorry", BUF_SIZE);
+			continue;
+		}
+
+
+		pthread_mutex_lock(&mutx);
+
+		clnt_socks[clnt_cnt]=clnt_sock;
+		read(clnt_sock, clnt_name, NAME_SIZE);		
+		strcpy(clnt_names[clnt_cnt++], clnt_name);
+		// ㄴ 클라이언트로부터 받은 접속자 이름입력
+		pthread_mutex_unlock(&mutx);
+
+		
+	
+		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+		pthread_detach(t_id);
+		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+		printf("Connected client name : %s \n", clnt_name);
+	}
+	close(serv_sock);
+	return 0;
+}
+	
+void * handle_clnt(void * arg)
+{
+	int clnt_sock=*((int*)arg);
+	int str_len=0, i;
+	int fSize = 0;
+	const char sig_file[BUF_SIZE] = {"send file"};
+	const char Fmsg_end[BUF_SIZE] = {"file end"};
+	char msg[BUF_SIZE] = {NULL};
+	char file_msg[BUF_SIZE] = {NULL};
+	char setfname[BUF_SIZE] = {NULL};
+
+	while((str_len=read(clnt_sock, msg, BUF_SIZE))!=0) 
+	{
+
+		if(!strcmp(msg, sig_file))
+		{
+			int j;
+			int fileGo = NULL;
+			char tmpName[NAME_SIZE]= {NULL};
+
+			//receive file size, name
+			read(clnt_sock, &fSize, sizeof(int));
+			printf("File size %d Byte\n", fSize);
+			read(clnt_sock, setfname, BUF_SIZE);
+			printf("received file name : %s\n",setfname);
+			
+			FILE *fp;
+			fp = fopen(setfname, "wb"); 
+		
+			while(1)
+			{		
+				read(clnt_sock, file_msg, BUF_SIZE);
+				if(!strcmp(file_msg, Fmsg_end)) 
+					break;
+				fwrite(file_msg, 1, BUF_SIZE, fp);
+			}
+
+			fclose(fp);
+			/////////////////////
+
+
+			pthread_mutex_unlock(&mutx);
+			printf("(!Notice)File data transfered \n");
+
+		} // ㄴ 파일전송
+	}
+
+	
+	pthread_mutex_lock(&mutx);
+	for(i=0; i<clnt_cnt; i++)   // remove disconnected client
+	{
+		if(clnt_sock==clnt_socks[i])
+		{
+			while(i++<clnt_cnt-1) {
+				clnt_socks[i]=clnt_socks[i+1];
+				strcpy(clnt_names[i], clnt_names[i+1]);
+			}
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+void error_handling(char * msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
